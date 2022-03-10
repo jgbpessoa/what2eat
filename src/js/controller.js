c74afefee2dea2db14aa29cce4e312bbec17079e
jgbpessoa <jgbpessoa@gmail.com>
2022-03-10 14:01:06 -0600

    recipe html/scss and start btn logic

--- src/js/controller.js
+++ src/js/controller.js
@@ -5,23 +5,48 @@ const btnStart = document.querySelector(".btn--start");
 const headerTitle = document.querySelector(".title");
 const headerSearch = document.querySelector(".header__search");
 const results = document.querySelector(".results");
-const recipe = document.querySelector(".recipe");
+const container = document.querySelector(".container");
+const navbar = document.querySelector(".nav");
+const welcome = document.querySelector(".welcome");
 
 btnStart.addEventListener("click", function () {
   console.log("btn clicked");
   headerTitle.classList.add("title--mobile");
   headerSearch.classList.add("header__search--mobile");
-  results.classList.add("show");
-  recipe.classList.add("hidden");
+  welcome.classList.add("hidden");
+  container.classList.add("padding-mobile");
+  results.classList.add("show-scale");
+  navbar.classList.add("show");
 });
 
 const showRecipe = async function () {
   try {
+    // Loading Recipe
     const res = await fetch(
       "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
     );
-    console.log(res);
-  } catch {
+
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    let { recipe } = data.data;
+
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+
+    console.log(recipe);
+
+    // Rendenring recipe
+    const markup = ``;
+  } catch (err) {
     alert(err);
   }
 };
