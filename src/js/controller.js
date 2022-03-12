10922dbc60af7c20ecf4b562145ddd3fedf23589
jgbpessoa <jgbpessoa@gmail.com>
2022-03-12 09:09:43 -0600

    started mvc architecture

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,6 @@
 import "babel-polyfill";
+import * as model from "./model.js";
+import recipeView from "./views/recipeView.js";
 
 // Variables
 const btnStart = document.querySelector(".btn--start");
@@ -6,58 +8,48 @@ const headerTitle = document.querySelector(".title");
 const headerSearch = document.querySelector(".header__search");
 const results = document.querySelector(".results");
 const container = document.querySelector(".container");
+const recipeContainer = document.querySelector(".recipe");
 const navbar = document.querySelector(".nav");
 const welcome = document.querySelector(".welcome");
 
 btnStart.addEventListener("click", function () {
   console.log("btn clicked");
   headerTitle.classList.add("title--mobile");
-  headerSearch.classList.add("header__search--mobile");
+  headerSearch.classList.add("show-scale");
   welcome.classList.add("hidden");
   container.classList.add("padding-mobile");
-  results.classList.add("show-scale");
+  // results.classList.add("show-scale");
+  recipeContainer.classList.add("show-scale");
   navbar.classList.add("show");
 });
 
-const showRecipe = async function () {
+const controlRecipes = async function () {
+  recipeView.renderSpinner();
   try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
     // Loading Recipe
-    const res = await fetch(
-      "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
-    );
-
-    const data = await res.json();
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-
-    let { recipe } = data.data;
-
-    recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
-    };
-
-    console.log(recipe);
+    await model.loadRecipe(id);
 
     // Rendenring recipe
-    const markup = ``;
+    recipeView.render(model.state.recipe);
   } catch (err) {
     alert(err);
   }
 };
 
-showRecipe();
 // Fixing mobile browsers 100vh issue
 const appHeight = () => {
   const doc = document.documentElement;
   doc.style.setProperty("--app-height", `${window.innerHeight}px`);
 };
 window.addEventListener("resize", appHeight);
+
+["hashchange", "load"].forEach((ev) =>
+  window.addEventListener(ev, controlRecipes)
+);
 
 const init = function () {
   appHeight();
---
+++ src/js/views/recipeView.js
@@ -0,0 +1,140 @@
+import fracty from "fracty";
+
+class RecipeView {
+  #parentElement = document.querySelector(".recipe");
+  #data;
+
+  render(data) {
+    this.#data = data;
+    const markup = this.#generateMarkup();
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML("afterbegin", markup);
+  }
+
+  renderSpinner = function () {
+    const markup = `
+      <div class="spinner">
+        <i class="fa-solid fa-spinner fa-spin"></i>
+      </div>
+    `;
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML("afterbegin", markup);
+  };
+
+  #clear() {
+    this.#parentElement.innerHTML = "";
+  }
+
+  #generateMarkup() {
+    return `<figure class="recipe__fig">
+            <img
+              src="${this.#data.image}"
+              alt="${this.#data.title}"
+              class="recipe__img"
+            />
+            <button class="btn--bookmark">
+              <span class="">
+                <i class="fa-regular fa-bookmark"></i>
+              </span>
+            </button>
+            <button class="btn--back">
+              <span class="">
+                <i class="fa-solid fa-arrow-left"></i>
+              </span>
+            </button>
+            <h1 class="recipe__title">
+              <span>${this.#data.title}</span>
+              <span class="recipe__user-generated">
+                <i class="fa-solid fa-user"></i>
+              </span>
+            </h1>
+          </figure>
+
+          <div class="recipe__details">
+            <div class="recipe__info">
+              <span class="recipe__info-icon">
+                <i class="fa-solid fa-clock"></i>
+              </span>
+              <span class="recipe__info-data recipe__info-data--minutes"
+                >${this.#data.cookingTime}</span
+              >
+              <span class="recipe__info-text">minutes</span>
+            </div>
+            <div class="recipe__info">
+              <span class="recipe__info-icon">
+                <i class="fa-solid fa-users"></i>
+              </span>
+              <span class="recipe__info-data recipe__info-data--people">${
+                this.#data.servings
+              }</span>
+              <span class="recipe__info-text">servings</span>
+
+              <div class="recipe__info-buttons">
+                <button class="btn--tiny btn--tiny-servings">
+                  <span class="btn--tiny-icon">
+                    <i class="fa-solid fa-circle-minus"></i>
+                  </span>
+                </button>
+                <button class="btn--tiny btn--tiny-servings">
+                  <span class="btn--tiny-icon">
+                    <i class="fa-solid fa-circle-plus"></i>
+                  </span>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div class="recipe__ingredients">
+            <h2 class="heading--2">Recipe ingredients</h2>
+            <ul class="recipe__ingredient-list">
+            ${this.#data.ingredients
+              .map(this.#generateMarkupIngredient)
+              .join("")}  
+            
+            </ul>
+          </div>
+
+          <div class="recipe__directions">
+            <h2 class="heading--2">How to cook it</h2>
+            <p class="recipe__directions-text">
+              This recipe was carefully designed and tested by
+              <span class="recipe__publisher">${
+                this.#data.publisher
+              }</span>. Please
+              check out directions at their website.
+            </p>
+            <a
+              class="btn--recipe"
+              href="${this.#data.sourceUrl}"
+              target="_blank"
+            >
+              <span>Directions</span>
+              <span class="search__icon">
+                <i class="fa-solid fa-arrow-right"></i>
+              </span>
+            </a>
+          </div>`;
+  }
+
+  #generateMarkupIngredient(ing) {
+    return `<li class="recipe__ingredient">
+                <span class="recipe__icon">
+                  <i class="fa-solid fa-check"></i>
+                </span>
+                <span class="recipe__quantity">${
+                  (ing.quantity && fracty(ing.quantity)) || " "
+                }</span>
+                <span class="recipe__description">
+                  <span class="recipe__unit">${ing.unit}</span>
+                  ${ing.description}
+                </span>
+                <button class="btn--tiny btn--tiny-cart">
+                  <span class="recipe__icon">
+                    <i class="fa-solid fa-cart-plus"></i>
+                  </span>
+                </button>
+              </li>`;
+  }
+}
+
+export default new RecipeView();
