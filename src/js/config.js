c13293e74d42ac05585f936664fcf4bacdc35809
jgbpessoa <jgbpessoa@gmail.com>
2022-04-01 08:54:28 -0500

    bug fix: input cursor not showing on chrome

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 8;
-export const KEY = "a1552466-9321-450d-82fd-1bf33e294ef1";
+export const KEY = "cc4a2727-a41f-4bf0-972e-d9b6438135f8";
 export const MODAL_CLOSE_SEC = 2.5;
